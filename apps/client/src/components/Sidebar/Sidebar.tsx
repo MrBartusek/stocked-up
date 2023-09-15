afc46f9e4dff4a4d5ab2f72d7d5f126d4b70e560
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-09-15 17:39:40 +0200

    Fix sidebar shrinking

--- apps/client/src/components/Sidebar/Sidebar.tsx
+++ apps/client/src/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,9 @@ import StockedUpLogo from '../StockedUpLogo';
 import WarehouseSelect from '../WarehouseSelect';
 
 function Sidebar() {
-	const Separator = <div className="mb-3 border-b border-gray-300 pb-3"></div>;
+	const Separator = <div className="mb-3 border-b border-gray-300 pb-4"></div>;
 	return (
-		<div className="h-100 flex w-72 flex-col border-r border-gray-300 bg-gray-150 px-8 py-6">
+		<div className="h-100 flex w-72 flex-shrink-0 flex-col border-r border-gray-300 bg-gray-150 px-8 py-6">
 			<div>
 				<StockedUpLogo
 					variant="black"
