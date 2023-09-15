5dbcb4e405f95eec9c6e41bd295fd9dddc920d23
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-09-15 17:31:59 +0200

    Add sidebar separator

--- apps/client/src/components/Sidebar/Sidebar.tsx
+++ apps/client/src/components/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import StockedUpLogo from '../StockedUpLogo';
 import WarehouseSelect from '../WarehouseSelect';
 
 function Sidebar() {
+	const Separator = <div className="mb-3 border-b border-gray-300 pb-3"></div>;
 	return (
 		<div className="h-100 flex w-72 flex-col border-r border-gray-300 bg-gray-150 px-8 py-6">
 			<div>
@@ -12,6 +13,7 @@ function Sidebar() {
 				/>
 			</div>
 			<WarehouseSelect />
+			{Separator}
 			<SidebarNavigation />
 		</div>
 	);
