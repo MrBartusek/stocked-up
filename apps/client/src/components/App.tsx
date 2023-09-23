023bbe317c10d755f66d7ea04b5392a2eb702aca
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-09-23 16:48:53 +0200

    Revert "Add simple auth0 login"
    This reverts commit d53ef51669dcd9d68058f90b063b20991bb1f1aa.

--- apps/client/src/components/App.tsx
+++ apps/client/src/components/App.tsx
@@ -1,21 +1,12 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import StockedUp from './StockedUp';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Auth0Provider
-				domain="stocked-up.eu.auth0.com"
-				clientId="cyal23cR9Q2zD6mcWF9nFJaRMA74SoT6"
-				authorizationParams={{
-					redirect_uri: window.location.origin,
-				}}
-			>
-				<StockedUp />
-			</Auth0Provider>
+			<StockedUp />
 		</QueryClientProvider>
 	);
 }
--- apps/client/src/components/Router.tsx
+++ apps/client/src/components/Router.tsx
@@ -4,14 +4,9 @@ import DashboardPage from './Pages/DashboardPage';
 import ProductViewPage from './Pages/ProductViewPage';
 import ProductsPage from './Pages/ProductsPage';
 import WarehousesPage from './Pages/WarehousesPage';
-import HomePage from './Pages/HomePage';
 
 function Router() {
 	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <HomePage />,
-		},
 		{
 			path: '/dashboard',
 			element: <DashboardPage />,
