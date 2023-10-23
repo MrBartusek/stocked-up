6c3d9b9755606c8ca94ea917442b6f0b8e14d3c6
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-10-23 19:45:32 +0200

    Add login page

--- apps/client/src/components/FullFocusContainer.tsx
+++ apps/client/src/components/FullFocusContainer.tsx
@@ -7,8 +7,13 @@ export interface FullFocusContainerProps {
 
 function FullFocusContainer({ children, className }: FullFocusContainerProps) {
 	return (
-		<div className="full-focus-background flex h-full w-full items-center justify-center bg-gray-900">
-			<div className={classNames(className, 'bg-light rounded-xl border border-gray-300 p-16')}>
+		<div className="flex h-full w-full items-center justify-center bg-gray-300">
+			<div
+				className={classNames(
+					className,
+					'rounded-xl border border-gray-300 bg-light p-16 shadow-xl',
+				)}
+			>
 				{children}
 			</div>
 		</div>
--- apps/client/src/components/Pages/LoginPage.tsx
+++ apps/client/src/components/Pages/LoginPage.tsx
@@ -1,14 +1,24 @@
-import { BsBoxSeam } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import UndrawTwoFactor from '../../assets/undraw_two_factor.svg';
 import FullFocusContainer from '../FullFocusContainer';
 
 function LoginPage() {
 	return (
-		<FullFocusContainer className="flex flex-col items-center">
-			<BsBoxSeam
-				className="mb-8 text-primary"
-				size={64}
-			/>
-			<h2 className="text-3xl">Login to StockedUp</h2>
+		<FullFocusContainer className="flex flex-row items-center gap-20">
+			<div className="w-[50%]">
+				<img
+					className="w-96"
+					src={UndrawTwoFactor}
+				></img>
+			</div>
+			<div className="flex w-[50%] flex-col">
+				<h2 className="mb-2 text-4xl">Login to your account</h2>
+				<div className="flex flex-row justify-center gap-1">
+					<span>Don&apos;t have an account?</span> <Link to="singup">Signup for free</Link>
+				</div>
+
+				<form></form>
+			</div>
 		</FullFocusContainer>
 	);
 }
