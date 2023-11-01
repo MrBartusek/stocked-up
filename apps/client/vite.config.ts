749bd092c58c5fa59da33bc18a35884c3ce84838
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-11-01 14:21:43 +0100

    Fix types compilation issue

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -12,4 +12,7 @@ export default defineConfig({
 			},
 		},
 	},
+	optimizeDeps: {
+		include: ['shared-types'],
+	},
 });
---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,15 @@
+import { ProductDto } from './ProductDto'
+import { BasicWarehouseDto } from './BasicWarehouseDto'
+import { UserDto } from './UserDto'
+import { SimpleResponseDto } from './SimpleResponseDto'
+import { UserLoginDto } from './auth/UserLoginDto'
+import { UserRegisterDto } from './auth/UserRegisterDto'
+
+export {
+    ProductDto,
+    BasicWarehouseDto,
+    UserDto,
+    SimpleResponseDto,
+    UserLoginDto,
+    UserRegisterDto
+}
