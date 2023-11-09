86c5fc49ab01a91e8fae1bee1dba07d2ef7215d7
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-11-09 12:50:54 +0100

    Minor client fixes

--- apps/client/src/components/DashboardContent.tsx
+++ apps/client/src/components/DashboardContent.tsx
@@ -8,12 +8,15 @@ export interface DashboardContentProps {
 
 function DashboardContent({ children, header }: DashboardContentProps) {
 	return (
-		<div className="flex-1 p-8">
+		<div className="flex-1 overflow-y-scroll  p-8">
 			<div className="flex items-center justify-between pb-8">
 				<h2 className="block text-3xl">{header}</h2>
 				<UserInfo />
 			</div>
 			<div className="mt-2">{children}</div>
+			<div className="mt-8 text-center text-sm text-muted">
+				StockedUp &copy; {new Date().getFullYear()}
+			</div>
 		</div>
 	);
 }
--- apps/client/src/components/Sidebar/Sidebar.tsx
+++ apps/client/src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import WarehouseSelect from '../WarehouseSelect';
 function Sidebar() {
 	const Separator = <div className="mb-3 border-b border-gray-300 pb-4"></div>;
 	return (
-		<div className="h-100 flex w-72 flex-shrink-0 flex-col border-r border-gray-300 bg-gray-150 px-8 py-6">
+		<div className="h-100 flex w-72 flex-shrink-0 flex-col overflow-y-auto border-r border-gray-300 bg-gray-150 px-8 py-6">
 			<div>
 				<StockedUpLogo
 					variant="black"
