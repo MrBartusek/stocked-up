f11bbec79f0ba2d0730d86a410e57e69da591c83
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-11-22 15:37:43 +0100

    Fix ci

--- apps/api/src/models/inventory/inventory.service.spec.ts
+++ apps/api/src/models/inventory/inventory.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InventoryService } from './inventory.service';
 
 describe('InventoryService', () => {
-  let service: InventoryService;
+	let service: InventoryService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [InventoryService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [InventoryService],
+		}).compile();
 
-    service = module.get<InventoryService>(InventoryService);
-  });
+		service = module.get<InventoryService>(InventoryService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
--- apps/api/src/models/warehouses/warehouses.module.ts
+++ apps/api/src/models/warehouses/warehouses.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { WarehousesService } from './warehouses.service';
-import { WarehousesController } from './warehouses.controller';
-import { WarehouseRepository } from './warehouse.repository';
-import { Warehouse, WarehouseSchema } from './schemas/warehouse.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { OrganizationsModule } from '../organizations/organizations.module';
+import { Warehouse, WarehouseSchema } from './schemas/warehouse.schema';
+import { WarehouseRepository } from './warehouse.repository';
+import { WarehousesController } from './warehouses.controller';
+import { WarehousesService } from './warehouses.service';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: Warehouse.name, schema: WarehouseSchema }])],
