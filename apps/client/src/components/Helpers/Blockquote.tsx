aa4a30fd27bb9b4191ae3679d5d8ad67bf4106af
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-03 13:46:22 +0100

    Update blockquote to use text-muted

--- apps/client/src/components/Helpers/Blockquote.tsx
+++ apps/client/src/components/Helpers/Blockquote.tsx
@@ -5,6 +5,10 @@ export interface BlockquoteProps {
 }
 
 function Blockquote({ children }: BlockquoteProps) {
-	return <blockquote className="mb-12 border-l-4 border-gray-300 py-4 ps-3">{children}</blockquote>;
+	return (
+		<blockquote className="mb-12 border-l-4 border-gray-300 py-4 ps-3 text-muted">
+			{children}
+		</blockquote>
+	);
 }
 export default Blockquote;
