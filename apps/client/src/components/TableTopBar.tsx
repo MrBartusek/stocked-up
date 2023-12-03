c1e41bc434f4ffa277cafb8981f358c9938d56f1
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-03 13:11:54 +0100

    Update table top padding

--- apps/client/src/components/TableTopBar.tsx
+++ apps/client/src/components/TableTopBar.tsx
@@ -5,7 +5,7 @@ export interface TableTopBarProps {
 
 function TableTopBar({ header, children }: TableTopBarProps) {
 	return (
-		<div className="mb-8 flex items-center justify-between border-b border-gray-200 pb-8">
+		<div className="mb-8 flex items-center justify-between border-b border-gray-200 pb-6">
 			<p>{header}</p>
 			{children}
 		</div>
