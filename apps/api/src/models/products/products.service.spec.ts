4656901fb4e47fc57dc54ce36abc9479d5386ba9
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-08 16:39:25 +0100

    product service tests

--- apps/api/src/models/products/products.service.spec.ts
+++ apps/api/src/models/products/products.service.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { ProductsRepository } from './products.repository';
+import { Types } from 'mongoose';
+import { CreateProductDto } from 'shared-types';
 
 describe('ProductsService', () => {
 	let service: ProductsService;
 
+	const regularMockFindFunction = (id?: Types.ObjectId) => {
+		return {
+			_id: id,
+			name: 'test-product',
+			buyPrice: 10,
+			sellPrice: 10,
+		};
+	};
+
+	const mockProductsRepository = {
+		create: jest.fn((dto: CreateProductDto) => {
+			return dto;
+		}),
+		find: jest.fn(() => new Array(10).fill(() => regularMockFindFunction(new Types.ObjectId()))),
+		findById: jest.fn((id: Types.ObjectId) => regularMockFindFunction(id)),
+		deleteOneById: jest.fn((id: Types.ObjectId) => regularMockFindFunction(id)),
+		exist: jest.fn(() => true),
+		findOneByIdAndUpdate: jest.fn((id: Types.ObjectId, query: any) => ({
+			...regularMockFindFunction(id),
+			...query,
+		})),
+		countDocuments: jest.fn(() => 100),
+	};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [ProductsService],
+			providers: [
+				ProductsService,
+				{
+					provide: ProductsRepository,
+					useValue: mockProductsRepository,
+				},
+			],
 		}).compile();
 
 		service = module.get<ProductsService>(ProductsService);
@@ -14,5 +47,63 @@ describe('ProductsService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('should create product', () => {
+		const product = service.create({
+			organizationId: '123',
+			name: 'created-product',
+		});
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: 'created-product',
+			}),
+		);
+	});
+
+	it('should update product', () => {
+		const product = service.update(new Types.ObjectId(), {
+			organizationId: '123',
+			name: 'updated-product',
+		});
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: 'updated-product',
+			}),
+		);
+	});
+
+	it('should delete product', () => {
+		const product = service.delete(new Types.ObjectId());
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: expect.any(String),
+			}),
+		);
+	});
+
+	it('should find one product', () => {
+		const product = service.findOne(new Types.ObjectId());
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: expect.any(String),
+			}),
+		);
+	});
+
+	it('should find all products', () => {
+		const products = service.findAll(new Types.ObjectId());
+		expect(products).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({
+					name: expect.any(String),
+				}),
+			]),
+		);
+	});
+
+	it('should count all products', () => {
+		const count = service.countAll(new Types.ObjectId());
+		expect(count).toBeGreaterThan(1);
 	});
 });
