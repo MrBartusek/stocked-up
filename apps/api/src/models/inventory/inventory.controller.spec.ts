ba834512402777ecc0487f5427d324343496fe05
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-10 12:13:08 +0100

    Start inventory controlelr tests

--- apps/api/src/models/inventory/inventory.controller.spec.ts
+++ apps/api/src/models/inventory/inventory.controller.spec.ts
@@ -5,15 +5,74 @@ import { WarehousesService } from '../warehouses/warehouses.service';
 import { ProductsService } from '../products/products.service';
 import { OrganizationsService } from '../organizations/organizations.service';
 import { OrganizationsStatsService } from '../organizations/organizations-stats.service';
+import { Types } from 'mongoose';
+import { OrgValueCalculationStrategy } from '../organizations/schemas/org-settings';
+
+const MOCK_IDS = {
+	inventory: {
+		taken: new Types.ObjectId('62a23958e5a9e9b88f853a67'),
+		free: new Types.ObjectId('62a23958e5a9e9b88f853a68'),
+	},
+	product: {
+		taken: new Types.ObjectId('62a23958e5a9e9b88f853a69'),
+		free: new Types.ObjectId('62a23958e5a9e9b88f853a6a'),
+	},
+	organization: {
+		taken: new Types.ObjectId('62a23958e5a9e9b88f853a6b'),
+		free: new Types.ObjectId('62a23958e5a9e9b88f853a6c'),
+	},
+	warehouse: {
+		taken: new Types.ObjectId('62a23958e5a9e9b88f853a6d'),
+		free: new Types.ObjectId('62a23958e5a9e9b88f853a6e'),
+	},
+};
 
 describe('InventoryController', () => {
 	let controller: InventoryController;
 
-	const mockInventoryService = {};
-	const mockWarehousesService = {};
-	const mockProductsService = {};
-	const mockOrganizationsService = {};
-	const mockOrganizationsStatsService = {};
+	const getMockProduct = (id: Types.ObjectId, aggregate = false) => {
+		return {
+			_id: id,
+			warehouse: MOCK_IDS.warehouse.taken,
+			quantity: 0,
+			location: 'test-location',
+			product: aggregate
+				? {
+						_id: MOCK_IDS.product.taken,
+						name: 'test-product',
+						buyPrice: 10,
+						sellPrice: 10,
+				  }
+				: MOCK_IDS.product.taken,
+		};
+	};
+
+	const mockInventoryService = {
+		create: jest.fn((id: Types.ObjectId) => getMockProduct(id, false)),
+		calculateTotalWarehouseValue: jest.fn(() => 100),
+	};
+
+	const mockWarehousesService = {
+		exist: jest.fn((id: Types.ObjectId) => id.toString() == MOCK_IDS.warehouse.taken.toString()),
+		updateTotalValue: jest.fn(),
+	};
+
+	const mockProductsService = {
+		exist: jest.fn((id: Types.ObjectId) => id.toString() == MOCK_IDS.product.taken.toString()),
+	};
+
+	const mockOrganizationsService = {
+		findByWarehouse: jest.fn(() => ({
+			settings: {
+				valueCalculationStrategy: OrgValueCalculationStrategy.BuyPrice,
+			},
+		})),
+		calculateTotalValue: jest.fn(() => 100),
+	};
+
+	const mockOrganizationsStatsService = {
+		updateTotalValue: jest.fn(() => 100),
+	};
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -46,7 +105,18 @@ describe('InventoryController', () => {
 	});
 
 	describe('Create inventory', () => {
-		test.todo('should create inventory item');
+		it('should create inventory item', async () => {
+			const product = await controller.create({
+				warehouseId: MOCK_IDS.warehouse.taken.toString(),
+				productId: MOCK_IDS.product.taken.toString(),
+			});
+
+			expect(product).toEqual(
+				expect.objectContaining({
+					productId: expect.any(Types.ObjectId),
+				}),
+			);
+		});
 
 		test.todo('should not create inventory item when warehouse does not exist');
 
