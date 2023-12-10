29a826cab1f7e1bd2e84194fc4ce45db7ff3cd18
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-10 11:32:05 +0100

    Add placeholder tests

--- apps/api/src/models/inventory/inventory.controller.spec.ts
+++ apps/api/src/models/inventory/inventory.controller.spec.ts
@@ -1,20 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { WarehousesService } from '../warehouses/warehouses.service';
+import { ProductsService } from '../products/products.service';
+import { OrganizationsService } from '../organizations/organizations.service';
+import { OrganizationsStatsService } from '../organizations/organizations-stats.service';
 
 describe('InventoryController', () => {
 	let controller: InventoryController;
 
+	const mockInventoryService = {};
+	const mockWarehousesService = {};
+	const mockProductsService = {};
+	const mockOrganizationsService = {};
+	const mockOrganizationsStatsService = {};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [InventoryController],
-			providers: [InventoryService],
-		}).compile();
+			providers: [
+				InventoryService,
+				WarehousesService,
+				ProductsService,
+				OrganizationsService,
+				OrganizationsStatsService,
+			],
+		})
+			.overrideProvider(InventoryService)
+			.useValue(mockInventoryService)
+			.overrideProvider(WarehousesService)
+			.useValue(mockWarehousesService)
+			.overrideProvider(ProductsService)
+			.useValue(mockProductsService)
+			.overrideProvider(OrganizationsService)
+			.useValue(mockOrganizationsService)
+			.overrideProvider(OrganizationsStatsService)
+			.useValue(mockOrganizationsStatsService)
+			.compile();
 
 		controller = module.get<InventoryController>(InventoryController);
 	});
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	describe('Create inventory', () => {
+		test.todo('should create inventory item');
+
+		test.todo('should not create inventory item when warehouse does not exist');
+
+		test.todo('should not create inventory item when product does not exist');
+	});
+
+	describe('Update inventory', () => {
+		test.todo('should update inventory item');
+
+		test.todo('should not update inventory item that does not exist');
+	});
+
+	describe('Delete inventory', () => {
+		test.todo('should delete inventory item');
+
+		test.todo('should not delete inventory item that does not exist');
+	});
+
+	describe('Find all inventory items', () => {
+		test.todo('should find all inventory item in warehouse');
+
+		test.todo('should return empty list if warehouse does not exist');
+	});
+
+	describe('Find one inventory items', () => {
+		test.todo('should find one inventory item');
+
+		test.todo('should not find item that does not exist');
 	});
 });
--- apps/api/src/models/organizations/organizations.controller.spec.ts
+++ apps/api/src/models/organizations/organizations.controller.spec.ts
@@ -1,20 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
+import { OrganizationsStatsService } from './organizations-stats.service';
+import { WarehousesService } from '../warehouses/warehouses.service';
 
 describe('OrganizationsController', () => {
 	let controller: OrganizationsController;
 
+	const mockWarehousesService = {};
+	const mockOrganizationsService = {};
+	const mockOrganizationsStatsService = {};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [OrganizationsController],
-			providers: [OrganizationsService],
-		}).compile();
+			providers: [OrganizationsService, OrganizationsStatsService, WarehousesService],
+		})
+			.overrideProvider(WarehousesService)
+			.useValue(mockWarehousesService)
+			.overrideProvider(OrganizationsService)
+			.useValue(mockOrganizationsService)
+			.overrideProvider(OrganizationsStatsService)
+			.useValue(mockOrganizationsStatsService)
+			.compile();
 
 		controller = module.get<OrganizationsController>(OrganizationsController);
 	});
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	describe('Create organization', () => {
+		test.todo('should create organization');
+
+		test.todo('should not create organization with taken name');
+	});
+
+	describe('Find all organizations', () => {
+		test.todo('should find all organizations for user');
+
+		test.todo('should not find organizations for user that does not have any');
+	});
+
+	describe('Update organization', () => {
+		test.todo('should update organization');
+
+		test.todo('should not update organization that does not exist');
+	});
+
+	describe('Delete organization', () => {
+		test.todo('should delete organization');
+
+		test.todo('should not delete organization that does not exist');
+	});
+
+	describe('Find one organization', () => {
+		test.todo('should find organization');
+
+		test.todo('should not find organization that does not exist');
+	});
+
+	describe("List organization's warehouses", () => {
+		test.todo("should list organization's warehouses");
+
+		test.todo('should return empty list when no warehouses');
+	});
+
+	describe('Organization settings', () => {
+		test.todo('should update organization settings');
+
+		test.todo('should not update organization settings of org that does not exist');
 	});
 });
