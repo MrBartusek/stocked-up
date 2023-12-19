0799c00955c5ca3b5ea455020e908e4f6790e782
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-12-19 23:57:10 +0100

    Update image dto

--- packages/shared-types/src/ImageDto.ts
+++ packages/shared-types/src/ImageDto.ts
@@ -2,13 +2,5 @@ import { IsBase64, IsEmpty, IsOptional, isBase64 } from "class-validator";
 
 export class ImageDto {
     hasImage: boolean;
-
-    @IsOptional()
-    @IsBase64()
-    base64?: string;
-
-    // don't allow for sending url from frontend
-    @IsOptional()
-    @IsEmpty()
     url?: string;
 }
