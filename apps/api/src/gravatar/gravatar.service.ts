2954d3caf98dceb29bdf240de52e2920eed4ab39
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-01-10 18:21:34 +0100

    Fix gravatar sending default image

--- apps/api/src/gravatar/gravatar.service.ts
+++ apps/api/src/gravatar/gravatar.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import * as gravatar from 'gravatar';
 import * as sharp from 'sharp';
 
 @Injectable()
 export class GravatarService {
 	async getGravatarBuffer(email: string): Promise<Buffer | null> {
-		const url = gravatar.url(email, { protocol: 'https', size: '256' });
+		const url = gravatar.url(email, { protocol: 'https', size: '256', default: '404' });
 		return axios
 			.get(url, { responseType: 'arraybuffer' })
 			.then((res) => sharp(res.data).toBuffer())
-			.catch((error) => {
+			.catch((error: AxiosError) => {
 				if (error?.response?.status == 404) {
 					return null;
 				}
