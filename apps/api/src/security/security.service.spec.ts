d2bdf6b5ffef4e1615a0cbe59ba4e691a67b7b97
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-01-27 20:17:39 +0100

    Add security service tests

--- apps/api/src/security/security.service.spec.ts
+++ apps/api/src/security/security.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SecurityService } from './security.service';
+import { OrganizationsAclService } from '../models/organizations/organizations-acl.service';
+import { Types } from 'mongoose';
+import { OrganizationAclRole } from '../models/organizations/types/org-acl-role.type';
+
+const PRIVILEGED_USER_ID = new Types.ObjectId();
 
 describe('SecurityService', () => {
 	let service: SecurityService;
 
+	const mockAclService = {
+		getRule: jest.fn((org, user) => {
+			if (PRIVILEGED_USER_ID.equals(user)) {
+				return {
+					user,
+					role: OrganizationAclRole.ADMIN,
+				};
+			}
+			return null;
+		}),
+	};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [SecurityService],
+			providers: [
+				SecurityService,
+				{
+					provide: OrganizationsAclService,
+					useValue: mockAclService,
+				},
+			],
 		}).compile();
 
 		service = module.get<SecurityService>(SecurityService);
@@ -14,5 +37,21 @@ describe('SecurityService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('should get role of user with access to organization', () => {
+		expect(service.getUserRole(new Types.ObjectId(), PRIVILEGED_USER_ID)).resolves.toBe(
+			OrganizationAclRole.ADMIN,
+		);
+	});
+
+	it('should not get role of user with has no access to organization', () => {
+		expect(service.getUserRole(new Types.ObjectId(), new Types.ObjectId())).resolves.toBeNull();
+	});
+
+	it('should check org access', () => {
+		expect(
+			service.hasOrganizationAccess(new Types.ObjectId(), PRIVILEGED_USER_ID),
+		).resolves.toBeTruthy();
 	});
 });
