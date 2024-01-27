2bf3d21f894fb30a6017dcc8078bbb932a248e37
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-01-27 20:02:59 +0100

    fix products controller tests

--- apps/api/src/models/products/products.controller.spec.ts
+++ apps/api/src/models/products/products.controller.spec.ts
@@ -1,13 +1,13 @@
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Types } from 'mongoose';
+import { MockSecurityPipe } from '../../mocks/mock-security.pipe';
+import { HasProductAccessPipe } from '../../security/pipes/has-product-access.pipe';
+import { SecurityValidationPipe } from '../../security/pipes/security-validation.pipe';
 import { OrganizationsStatsService } from '../organizations/organizations-stats.service';
 import { MockProductsRepository } from './mocks/mock-products-repository';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-
-const MOCK_TAKEN_PRODUCT_ID = new Types.ObjectId('62a23958e5a9e9b88f853a67');
-const MOCK_FREE_PRODUCT_ID = new Types.ObjectId('657047c4e0cecd73abbad627');
+import { HasOrganizationAccessPipe } from '../../security/pipes/has-organization-access.pipe';
 
 describe('ProductsController', () => {
 	let controller: ProductsController;
@@ -16,19 +16,12 @@ describe('ProductsController', () => {
 
 	const mockProductsService = {
 		create: jest.fn((data) => mockProductsRepo.create(data)),
-		update: jest.fn((id: Types.ObjectId, query: any) => {
-			if (id != MOCK_TAKEN_PRODUCT_ID) return;
-			return mockProductsRepo.findOneByIdAndUpdate(id, query);
-		}),
-		findOne: jest.fn((id: Types.ObjectId) => {
-			if (id != MOCK_TAKEN_PRODUCT_ID) return;
-			return mockProductsRepo.findById(id);
-		}),
+		update: jest.fn((id: Types.ObjectId, query: any) =>
+			mockProductsRepo.findOneByIdAndUpdate(id, query),
+		),
+		findOne: jest.fn((id: Types.ObjectId) => mockProductsRepo.findById(id)),
 		paginate: jest.fn((query, page) => mockProductsRepo.paginate(query, page)),
-		delete: jest.fn((id: Types.ObjectId) => {
-			if (id != MOCK_TAKEN_PRODUCT_ID) return;
-			return mockProductsRepo.deleteOneById(id);
-		}),
+		delete: jest.fn((id: Types.ObjectId) => mockProductsRepo.deleteOneById(id)),
 		countAll: jest.fn(() => mockProductsRepo.countDocuments()),
 	};
 
@@ -49,6 +42,12 @@ describe('ProductsController', () => {
 			.useValue(mockProductsService)
 			.overrideProvider(OrganizationsStatsService)
 			.useValue(mockOrgStatService)
+			.overridePipe(SecurityValidationPipe)
+			.useValue(MockSecurityPipe)
+			.overridePipe(HasProductAccessPipe)
+			.useValue(MockSecurityPipe)
+			.overridePipe(HasOrganizationAccessPipe)
+			.useValue(MockSecurityPipe)
 			.compile();
 
 		controller = module.get<ProductsController>(ProductsController);
@@ -62,86 +61,56 @@ describe('ProductsController', () => {
 		expect(controller).toBeDefined();
 	});
 
-	describe('Create product', () => {
-		it('should create product', async () => {
-			const product = await controller.create({
-				organization: new Types.ObjectId().toString(),
+	it('should create product', async () => {
+		const product = await controller.create({
+			organization: new Types.ObjectId().toString(),
+			name: 'created-product',
+		});
+
+		expect(product).toEqual(
+			expect.objectContaining({
 				name: 'created-product',
-			});
-
-			expect(product).toEqual(
-				expect.objectContaining({
-					name: 'created-product',
-				}),
-			);
-			expect(updateProductsCountSpy).toHaveBeenCalled();
-		});
+			}),
+		);
+		expect(updateProductsCountSpy).toHaveBeenCalled();
 	});
 
-	describe('Update product', () => {
-		it('should update product', async () => {
-			const orgId = new Types.ObjectId();
-			const product = await controller.update(MOCK_TAKEN_PRODUCT_ID, {
-				organization: orgId.toString(),
+	it('should update product', async () => {
+		const orgId = new Types.ObjectId();
+		const product = await controller.update(new Types.ObjectId(), {
+			organization: orgId.toString(),
+			name: 'updated-product',
+			image: { hasImage: false },
+		});
+
+		expect(product).toEqual(
+			expect.objectContaining({
 				name: 'updated-product',
-				image: { hasImage: false },
-			});
-
-			expect(product).toEqual(
-				expect.objectContaining({
-					name: 'updated-product',
-				}),
-			);
-			expect(recalculateOrgSpy).toHaveBeenCalled();
-		});
-
-		it('should not update product that does not exist', async () => {
-			const product = controller.update(MOCK_FREE_PRODUCT_ID, {
-				organization: new Types.ObjectId().toString(),
-				name: 'updated-product',
-				image: { hasImage: false },
-			});
-
-			expect(product).rejects.toThrow(NotFoundException);
-		});
+			}),
+		);
+		expect(recalculateOrgSpy).toHaveBeenCalled();
 	});
 
-	describe('Find product', () => {
-		it('should find products', async () => {
-			const product = await controller.findOne(MOCK_TAKEN_PRODUCT_ID);
+	it('should find products', async () => {
+		const product = await controller.findOne(new Types.ObjectId());
 
-			expect(product).toEqual(
-				expect.objectContaining({
-					name: expect.any(String),
-				}),
-			);
-		});
-
-		it('should not find product that does not exist', async () => {
-			const product = controller.findOne(MOCK_FREE_PRODUCT_ID);
-
-			expect(product).rejects.toThrow(NotFoundException);
-		});
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: expect.any(String),
+			}),
+		);
 	});
 
-	describe('Delete product', () => {
-		it('should delete products', async () => {
-			const product = await controller.delete(MOCK_TAKEN_PRODUCT_ID);
+	it('should delete products', async () => {
+		const product = await controller.delete(new Types.ObjectId());
 
-			expect(product).toEqual(
-				expect.objectContaining({
-					name: expect.any(String),
-				}),
-			);
-			expect(updateProductsCountSpy).toHaveBeenCalled();
-			expect(recalculateOrgSpy).toHaveBeenCalled();
-		});
-
-		it('should not delete product that does not exist', async () => {
-			const product = controller.delete(MOCK_FREE_PRODUCT_ID);
-
-			expect(product).rejects.toThrow(NotFoundException);
-		});
+		expect(product).toEqual(
+			expect.objectContaining({
+				name: expect.any(String),
+			}),
+		);
+		expect(updateProductsCountSpy).toHaveBeenCalled();
+		expect(recalculateOrgSpy).toHaveBeenCalled();
 	});
 
 	it('should find all products by org', async () => {
