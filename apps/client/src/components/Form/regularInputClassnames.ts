a8a615e356027bf9095ff899c5f9eef4f0305196
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-06 11:50:12 +0100

    Update input fields

--- apps/client/src/components/Form/regularInputClassnames.ts
+++ apps/client/src/components/Form/regularInputClassnames.ts
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 
 const REGULAR_INPUT_CLASSNAMES = classNames(
-	'flex flex-1 bg-inherit px-4 py-2 outline-none disabled:bg-gray-100',
+	'flex flex-1 px-4 py-2 outline-none disabled:bg-gray-100',
 	'text-muted',
 	'rounded-md border border-gray-300 focus:border-primary ',
 );
--- apps/client/src/components/UserAvatar.tsx
+++ apps/client/src/components/UserAvatar.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import { UserDto } from 'shared-types';
 
 export interface UserAvatarProps {
-	user: UserDto;
+	user?: UserDto;
 	variant?: 'square' | 'circle';
 	size?: number;
 }
@@ -11,11 +11,17 @@ function UserAvatar({ user, variant = 'square', size = 6 }: UserAvatarProps) {
 	return (
 		<div style={{ width: `${size / 4}rem` }}>
 			<img
-				className={classNames('w-100', {
-					'rounded-full': variant == 'circle',
-					'rounded-md': variant == 'square',
-				})}
-				src={user.image.url}
+				height={64}
+				width={64}
+				className={classNames(
+					'w-100',
+					{
+						'rounded-full': variant == 'circle',
+						'rounded-md': variant == 'square',
+					},
+					'bg-gray-300',
+				)}
+				src={user?.image.url}
 			/>
 		</div>
 	);
