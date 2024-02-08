add2d251da9eddc9d4400b8c0d6d86580158b3f1
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-08 12:54:27 +0100

    Disable organization delete buttton for non owners

--- apps/client/src/components/Organization/OrganizationDangerZone.tsx
+++ apps/client/src/components/Organization/OrganizationDangerZone.tsx
@@ -12,8 +12,8 @@ function OrganizationDangerZone({ organization }: OrganizationDangerZoneProps) {
 			<h2 className="mb-4 text-2xl">Danger Zone</h2>
 			<p className="text-muted">Potentially destructive actions, be cautious!</p>
 			<div className="mt-6 inline-flex flex-col gap-6">
-				<OrganizationDeleteButton />
 				<OrganizationLeaveButton organization={organization} />
+				<OrganizationDeleteButton organization={organization} />
 			</div>
 		</div>
 	);
--- apps/client/src/components/Organization/OrganizationDeleteButton.tsx
+++ apps/client/src/components/Organization/OrganizationDeleteButton.tsx
@@ -1,17 +1,26 @@
 import { BsTrashFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { OrganizationDto } from 'shared-types';
+import useUserRole from '../../hooks/useUserRole';
 import IconButton from '../IconButton';
 
-function OrganizationDeleteButton() {
+export interface OrganizationDeleteButtonProps {
+	organization: OrganizationDto;
+}
+
+function OrganizationDeleteButton({ organization }: OrganizationDeleteButtonProps) {
+	const { role } = useUserRole(organization.id);
+	const navigate = useNavigate();
+
 	return (
-		<Link to="delete">
-			<IconButton
-				variant="danger"
-				icon={BsTrashFill}
-			>
-				Delete organization
-			</IconButton>
-		</Link>
+		<IconButton
+			variant="danger"
+			icon={BsTrashFill}
+			disabled={role != 'owner'}
+			onClick={() => navigate('delete')}
+		>
+			Delete organization
+		</IconButton>
 	);
 }
 export default OrganizationDeleteButton;
