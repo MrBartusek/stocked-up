4a3204e22b1433791209b15d60fffdf9ad0fc273
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-08 22:33:07 +0100

    Update EmailConfirmForm.tsx

--- apps/client/src/components/EmailConfirmForm.tsx
+++ apps/client/src/components/EmailConfirmForm.tsx
@@ -13,7 +13,7 @@ export interface EmailConfirmFormProps {
 }
 
 function EmailConfirmForm({ token }: EmailConfirmFormProps) {
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
 	const queryClient = useQueryClient();
 
