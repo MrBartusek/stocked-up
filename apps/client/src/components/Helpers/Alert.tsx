42cc50fe571a59e15b127db068a676b3b09dae43
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-14 11:16:32 +0100

    Add alert variants

--- apps/client/src/components/Helpers/Alert.tsx
+++ apps/client/src/components/Helpers/Alert.tsx
@@ -2,17 +2,26 @@ import classNames from 'classnames';
 
 type DivProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
+export type AlertVariant = 'danger' | 'success';
+
 export interface AlertProps extends DivProps {
 	children: React.ReactNode;
+	variant?: AlertVariant;
 }
 
-function Alert({ children, ...props }: AlertProps) {
+function Alert({ children, variant = 'danger', ...props }: AlertProps) {
+	const colorVariants: { [key in AlertVariant]: string } = {
+		danger: 'border-red-300 bg-red-200',
+		success: 'border-green-300 bg-green-200',
+	};
+
 	return (
 		<div
 			{...props}
 			className={classNames(
-				'overflow-hidden break-words rounded-sm border border-red-300 bg-red-200 p-4',
+				'overflow-hidden break-words rounded-sm border  p-4',
 				'flex items-center gap-2',
+				colorVariants[variant],
 				props.className,
 			)}
 		>
