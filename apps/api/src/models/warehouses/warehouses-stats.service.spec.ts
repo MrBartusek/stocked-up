de2a66f5730571fdd7270f01590de9d9603df0e6
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-25 17:01:48 +0100

    Fix organization stats service tests

--- apps/api/src/models/warehouses/warehouses-stats.service.spec.ts
+++ apps/api/src/models/warehouses/warehouses-stats.service.spec.ts
@@ -1,33 +1,22 @@
+import { getQueueToken } from '@nestjs/bull';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WarehouseStatsService } from './warehouse-stats.service';
 import { Types } from 'mongoose';
-import { InventoryStatsService } from '../inventory/inventory-stats.service';
-import { OrgValueCalculationStrategy } from '../organizations/schemas/org-settings';
-import { WarehouseRepository } from './warehouse.repository';
-import { WarehouseStatsService } from './warehouses-stats.service';
 
 describe('WarehouseStatsService', () => {
 	let service: WarehouseStatsService;
 
-	const mockWarehouseRepository = {
-		findOneByIdAndUpdate: jest.fn(() => {}),
+	const recalculateQueueMock = {
+		add: jest.fn(),
 	};
-	const mockInventoryStatsService = {
-		getTotalWarehouseInventoryValue: jest.fn(() => 100),
-	};
-
-	const findOneByIdAndUpdateSpy = jest.spyOn(mockWarehouseRepository, 'findOneByIdAndUpdate');
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [
 				WarehouseStatsService,
 				{
-					provide: WarehouseRepository,
-					useValue: mockWarehouseRepository,
-				},
-				{
-					provide: InventoryStatsService,
-					useValue: mockInventoryStatsService,
+					provide: getQueueToken('warehouse-recalculate'),
+					useValue: recalculateQueueMock,
 				},
 			],
 		}).compile();
@@ -35,23 +24,21 @@ describe('WarehouseStatsService', () => {
 		service = module.get<WarehouseStatsService>(WarehouseStatsService);
 	});
 
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
-
 	it('should be defined', () => {
 		expect(service).toBeDefined();
 	});
 
-	it('should recalculate warehouse value', async () => {
-		const warehouseId = new Types.ObjectId();
+	it('should add recalculate job', async () => {
+		const warehouse = new Types.ObjectId();
+		const expectedJobData = { warehouse };
 
-		const value = await service.recalculateWarehouseValue(
-			warehouseId,
-			OrgValueCalculationStrategy.BuyPrice,
+		await service.recalculateTotalValue(warehouse);
+
+		expect(recalculateQueueMock.add).toBeCalledWith(
+			expectedJobData,
+			expect.objectContaining({
+				jobId: expect.any(String),
+			}),
 		);
-
-		expect(value).toBe(100);
-		expect(findOneByIdAndUpdateSpy).toHaveBeenCalledWith(warehouseId, { totalValue: 100 });
 	});
 });
