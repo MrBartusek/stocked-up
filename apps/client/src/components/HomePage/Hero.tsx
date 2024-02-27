73ff2bb9821b8ef0f0ff998e2b102458c9f421eb
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-02-27 14:16:56 +0100

    Add more spacing to front page

--- apps/client/src/components/HomePage/Hero.tsx
+++ apps/client/src/components/HomePage/Hero.tsx
@@ -6,11 +6,11 @@ import HomePageActionButtons from './HomePageActionButtons';
 function Hero() {
 	return (
 		<div className="bg-gradient-to-b from-amber-50 to-transparent">
-			<Container className="flex flex-col items-center py-24">
+			<Container className="flex flex-col items-center py-28">
 				<div className="max-w-xl text-center">
 					<h1
 						className={classNames(
-							'bg-clip-text font-extrabold text-gray-900 text-transparent ',
+							'bg-clip-text font-extrabold text-gray-900',
 							'mb-8 text-4xl md:text-6xl',
 						)}
 					>
@@ -27,7 +27,7 @@ function Hero() {
 				<div
 					className={classNames(
 						'max-w-6xl overflow-hidden rounded-xl border border-gray-200 shadow-xl',
-						'mt-24 hidden lg:block',
+						'mt-36 hidden lg:block',
 					)}
 				>
 					<img
