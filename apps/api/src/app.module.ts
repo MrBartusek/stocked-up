27f5ae18d6709757abb12cebbda9d0b96e850ccc
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-09 20:17:59 +0200

    Revert "Add config module validation"
    This reverts commit ef65bafffb68f7c267a8bed518b60140eddccf11.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -21,8 +21,6 @@ import { RedisModule } from './redis/redis.module';
 import { S3CacheModule } from './s3-cache/s3-cache.module';
 import { S3Module } from './s3/s3.module';
 import { SecurityModule } from './security/security.module';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 
 const FrontendModule = ServeStaticModule.forRoot({
 	rootPath: join(__dirname, '../..', 'client', 'dist'),
@@ -39,25 +37,6 @@ const FrontendModule = ServeStaticModule.forRoot({
 				removeOnFail: true,
 				delay: 500,
 			},
-		}),
-		ConfigModule.forRoot({
-			isGlobal: true,
-			validationSchema: Joi.object({
-				NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
-				PORT: Joi.number().port().default(3000),
-				BASE_API_URL: Joi.string().uri().default('http://localhost:3000/api'),
-				SESSION_SECRET: Joi.string().min(4).required(),
-				REDIS_HOST: Joi.string().default('localhost'),
-				REDIS_PASS: Joi.string().allow(''),
-				REDIS_PORT: Joi.number().port().default(6379),
-				MONGO_URL: Joi.string().uri().default('mongodb://127.0.0.1:27017/stockedUp'),
-				AWS_ACCESS_KEY_ID: Joi.string().required(),
-				AWS_SECRET_ACCESS_KEY: Joi.string().required(),
-				AWS_DEFAULT_REGION: Joi.string().required(),
-				AWS_BUCKET_NAME: Joi.string().required(),
-				RESEND_API_KEY: Joi.string().required(),
-				EMAIL_SENDER: Joi.string().email().default('noreply@dokurno.dev'),
-			}),
 		}),
 		FrontendModule,
 		ProductsModule,
