fc7d35d30a6a2c8ceee9580a99da71c32c1f352f
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-11 17:00:34 +0200

    Update resend.factory.ts

--- apps/api/src/emails/resend.factory.ts
+++ apps/api/src/emails/resend.factory.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 import Utils from '../helpers/utils';
-import { ResendMock } from './mocks/resendMock';
+import { ResendMock } from './mocks/resend.mock';
 
 export class ResendFactory {
 	getInstance(key: string) {
