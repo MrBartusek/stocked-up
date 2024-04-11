d30119a5f69558103130a15f27962be7888296b8
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-11 17:20:22 +0200

    Add tests for gravatar service

--- apps/api/src/gravatar/gravatar.service.spec.ts
+++ apps/api/src/gravatar/gravatar.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GravatarService } from './gravatar.service';
+import * as sharp from 'sharp';
+
+// Both of these e-mails are controlled by me
+// So i can control if they are registered or not
+const VALID_EMAIL = 'admin@dokurno.dev';
+const INVALID_EMAIL = 'do-not-register@dokurno.dev';
 
 describe('GravatarService', () => {
 	let service: GravatarService;
@@ -14,5 +20,18 @@ describe('GravatarService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('should get avatar', async () => {
+		const avatar = await service.getGravatarBuffer(VALID_EMAIL);
+		const metadata = await sharp(avatar).metadata();
+
+		expect(metadata.height).toBeGreaterThan(10);
+		expect(metadata.width).toBeGreaterThan(10);
+	});
+
+	it('should return null on missing avatar', async () => {
+		const avatar = await service.getGravatarBuffer(INVALID_EMAIL);
+		expect(avatar).toBeNull();
 	});
 });
