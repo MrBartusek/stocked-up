037e5634dc68c8ea15c48cc4432729e79bca88f0
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-14 14:50:38 +0200

    Add `HasOwnerAccessPipe` tests

---
+++ apps/api/src/security/pipes/has-owner-access.pipe.spec.ts
@@ -0,0 +1,38 @@
+import { ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { mockUserRequest } from '../../mocks/mock-user-request';
+import { SecurityServiceMock } from '../mocks/security-service.mock';
+import { SecurityService } from '../security.service';
+import { HasOrganizationAccessPipe } from './has-organization-access.pipe';
+import { HasOwnerAccessPipe } from './has-owner-access.pipe';
+import { OrganizationSecurityRole } from 'shared-types';
+
+const VALID_ORG_ID = new Types.ObjectId();
+const ORG_OWNER = new Types.ObjectId();
+
+describe('HasOwnerAccessPipe', () => {
+	const securityServiceMock = {
+		...new SecurityServiceMock(VALID_ORG_ID),
+		getUserRole: jest.fn((org, userId) =>
+			ORG_OWNER.equals(userId) ? OrganizationSecurityRole.OWNER : OrganizationSecurityRole.MEMBER,
+		),
+	} as any as SecurityService;
+
+	it('should pass on owner', () => {
+		const request = { ...mockUserRequest } as any;
+		request.user.id = ORG_OWNER;
+
+		const pipe = new HasOwnerAccessPipe(request, securityServiceMock);
+		expect(pipe.transform(VALID_ORG_ID)).resolves.toStrictEqual(VALID_ORG_ID);
+	});
+
+	it('should fail on member', () => {
+		const request = { ...mockUserRequest } as any;
+		request.user.id = new Types.ObjectId();
+
+		const pipe = new HasOwnerAccessPipe(request, securityServiceMock);
+		expect(pipe.transform(VALID_ORG_ID)).rejects.toThrowError(ForbiddenException);
+	});
+});
