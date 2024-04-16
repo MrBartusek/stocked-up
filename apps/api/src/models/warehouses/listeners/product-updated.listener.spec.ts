55d91565a3b2571e8edbe1c60ef06579e37e90b0
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-16 13:01:23 +0200

    Add `ProductUpdatedListener` test

---
+++ apps/api/src/models/warehouses/listeners/product-updated.listener.spec.ts
@@ -0,0 +1,72 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OrganizationUpdatedEvent } from '../../organizations/events/organization-updated.event';
+import { OrganizationSettingsUpdatedListener } from './organization-settings-updated.listener';
+import { WarehouseStatsService } from '../warehouse-stats.service';
+import { OrganizationDocument } from '../../organizations/schemas/organization.schema';
+import { Types } from 'mongoose';
+import { ProductUpdatedListener } from './product-updated.listener';
+import { OrganizationsService } from '../../organizations/organizations.service';
+import { ProductUpdatedEvent } from '../../products/events/product-updated.event';
+import { ProductDocument } from '../../products/schemas/product.schema';
+
+describe('ProductUpdatedListener', () => {
+	let listener: ProductUpdatedListener;
+
+	const mockOrgsService = {
+		findById: jest.fn(),
+	};
+
+	const mockStatsService = {
+		recalculateTotalValue: jest.fn(),
+	};
+
+	const recalculateSpy = jest.spyOn(mockStatsService, 'recalculateTotalValue');
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				ProductUpdatedListener,
+				{
+					provide: WarehouseStatsService,
+					useValue: mockStatsService,
+				},
+				{
+					provide: OrganizationsService,
+					useValue: mockOrgsService,
+				},
+			],
+		}).compile();
+
+		listener = module.get<ProductUpdatedListener>(ProductUpdatedListener);
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should be defined', () => {
+		expect(listener).toBeDefined();
+	});
+
+	it('should recalculate on organization.settings.updated', async () => {
+		const orgId = new Types.ObjectId();
+		const org = {
+			_id: orgId,
+			warehouses: Array(5).fill(() => ({
+				id: new Types.ObjectId(),
+				name: `warehouse`,
+			})),
+		};
+
+		mockOrgsService.findById.mockImplementation((id) => {
+			return id == orgId ? org : null;
+		});
+		const product: ProductDocument = { organization: orgId } as any;
+		const event = new ProductUpdatedEvent(product);
+
+		await listener.handleProductUpdated(event);
+
+		expect(recalculateSpy).toBeCalledTimes(5);
+		expect(recalculateSpy).toBeCalledWith(org.warehouses[0].id);
+	});
+});
