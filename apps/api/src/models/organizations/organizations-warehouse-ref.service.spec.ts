e388687e50c6a65b77b1903dabd9499948a72598
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-16 15:30:24 +0200

    Add tests for `OrganizationsWarehouseRefService`

---
+++ apps/api/src/models/organizations/organizations-warehouse-ref.service.spec.ts
@@ -0,0 +1,80 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MockOrganizationsRepository } from './mocks/mock-organizations-repository';
+import { OrganizationsWarehouseRefService } from './organizations-warehouse-ref.service';
+import { OrganizationRepository } from './organizations.repository';
+import { Types } from 'mongoose';
+
+describe('OrganizationsWarehouseRefService', () => {
+	let service: OrganizationsWarehouseRefService;
+
+	const mockOrganizationRepository = new MockOrganizationsRepository();
+
+	const updateByIdSpy = jest.spyOn(mockOrganizationRepository, 'findOneByIdAndUpdate');
+	const updateSpy = jest.spyOn(mockOrganizationRepository, 'findOneAndUpdate');
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				OrganizationsWarehouseRefService,
+				{
+					provide: OrganizationRepository,
+					useValue: mockOrganizationRepository,
+				},
+			],
+		}).compile();
+
+		service = module.get<OrganizationsWarehouseRefService>(OrganizationsWarehouseRefService);
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	it('should add ref', async () => {
+		const orgId = new Types.ObjectId();
+		const warehouseId = new Types.ObjectId();
+
+		await service.addRef(orgId, {
+			id: warehouseId,
+			name: 'warehouse',
+		});
+
+		expect(updateByIdSpy).toBeCalledWith(orgId, {
+			$push: expect.anything(),
+		});
+	});
+
+	it('should update ref', async () => {
+		const orgId = new Types.ObjectId();
+		const warehouseId = new Types.ObjectId();
+
+		await service.updateRef(orgId, {
+			id: warehouseId,
+			name: 'warehouse',
+		});
+
+		expect(updateSpy).toBeCalledWith(
+			expect.objectContaining({
+				_id: orgId,
+			}),
+			{
+				$set: expect.anything(),
+			},
+		);
+	});
+
+	it('should update ref', async () => {
+		const orgId = new Types.ObjectId();
+		const warehouseId = new Types.ObjectId();
+
+		await service.deleteRef(orgId, warehouseId);
+
+		expect(updateByIdSpy).toBeCalledWith(orgId, {
+			$pull: expect.anything(),
+		});
+	});
+});
