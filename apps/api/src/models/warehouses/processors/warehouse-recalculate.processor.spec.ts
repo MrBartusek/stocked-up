78292a71ff183f4d2959527384d7c5964cd063ee
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-29 18:46:36 +0200

    Fix tests

--- apps/api/src/models/warehouses/processors/warehouse-recalculate.processor.spec.ts
+++ apps/api/src/models/warehouses/processors/warehouse-recalculate.processor.spec.ts
@@ -72,6 +72,7 @@ describe('WarehouseRecalculateProcessor', () => {
 			_id: orgId,
 			settings: {
 				valueCalculationStrategy: OrgValueCalculationStrategy.BuyPrice,
+				currency: 'USD',
 			},
 		};
 
