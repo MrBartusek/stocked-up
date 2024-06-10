244cb12ec3238fd8909928caa907e8d7be2752aa
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-06-10 12:59:27 +0200

    Add options to `setupTestUser` test utility

--- apps/api/test/setup-user.ts
+++ apps/api/test/setup-user.ts
@@ -1,8 +1,23 @@
 import * as request from 'supertest';
+import { CreateOrganizationDto } from '../src/models/organizations/dto/create-organization.dto';
 
-export async function setupTestUser(agent: request.SuperAgentTest) {
+export interface TestUserOptions {
+	createOrganization?: boolean;
+}
+
+export async function setupTestUser(agent: request.SuperAgentTest, options: TestUserOptions = {}) {
 	const payload = { username: 'test-user', email: 'test@dokurno.dev', password: 'Test123!' };
 
 	await agent.post('/api/auth/register').send(payload);
 	await agent.post('/api/auth/login').send(payload);
+
+	if (options.createOrganization == true) {
+		const dto: CreateOrganizationDto = {
+			name: 'auto-mock-test-org',
+			warehouse: {
+				name: 'auto-mock-test-warehouse',
+			},
+		};
+		await agent.post('/api/organizations').send(dto);
+	}
 }
